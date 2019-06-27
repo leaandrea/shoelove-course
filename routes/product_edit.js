956c407d78da66ed87065049780bf375115815df
Cecile Pham <sheshiru.van@gmail.com>
2019-06-27 15:08:04 +0200

    add poduct edit function and not found page

---
+++ routes/product_edit.js
@@ -0,0 +1,20 @@
+router.get("/prod_manage/:id/edit", (req, res) => {
+  Product.findById(req.params.id)
+    .then(updatedInfos => {
+      res.render("product_edit.hbs", { sneaker: updatedInfos });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.post("/product_edit/:id", (req, res) => {
+  Product.findByIdAndUpdate(req.params.id, req.body)
+    .then(dbRes => {
+      console.log("successfully edited sneaker", dbRes);
+      res.redirect("/prod-manage.hbs");
+    })
+    .catch(err => {
+      console.log("Oops, error as occured, update didn't worked", err);
+    });
+});
