df7bbe3bc29efcd9c46f4ed8966badae3c008e54
leaandrea <leaandreapaumier@gmail.com>
2019-06-27 17:28:02 +0200

    minor changes concerning edit - to modify

--- routes/product_edit.js
+++ routes/product_edit.js
@@ -4,8 +4,10 @@ const Product = require("../models/Product");
 
 router.get("/product_edit/:id", (req, res) => {
   Product.findById(req.params.id)
-    .then(updatedInfos => {
-      res.render("product_edit.hbs", { sneaker: updatedInfos });
+    .then(shoeId => {
+      console.log(shoeId);
+
+      res.render("product_edit.hbs", { shoeId });
     })
     .catch(err => {
       console.log(err);
