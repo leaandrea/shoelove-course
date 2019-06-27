5d55ead9a35b0dc8e288344346b88bda1737d9fc
Camille Jacques <camille.jacques75@gmail.com>
2019-06-27 15:11:36 +0200

    product and tag create

--- models/Tag.js
+++ models/Tag.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tagSchema = new Schema({
-  label: { type: String, enum: ["Friendly", "Urban style", "Dope"] }
+  label: String
 });
 
 const Tag = mongoose.model("Tag", tagSchema);
---
+++ routes/product_create.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = new express.Router();
+const Product = require("../models/Product");
+const Tag = require("../models/Tag");
+
+router.get("/prod-add", (req, res) => {
+  res.render("products_add")
+})
+
+router.post("/prod-add", (req, res) => {
+  const {name, ref, size, description, price, category, id_tag} = req.body;
+  Product.create({
+    name,
+    ref,
+    size,
+    description,
+    price,
+    category,
+    id_tag
+  })
+  .then (newShoe => {
+    console.log("new product added:", newShoe)
+    res.redirect("products_manage");
+  })
+  .catch(error => console.error("error creating product:", error))
+});
+
+router.post("/tag-add", (req, res) => {
+  const label = req.body;
+  Tag.create({label})
+  .then (newTag => {
+    console.log("new tag added:", newTag);
+    res.render("products_add");
+  })
+  .catch(error => {
+    console.log("error adding tag:", error);
+    res.render("products_add");
+  })
+});
+
+module.exports = router;
