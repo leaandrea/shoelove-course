317faffb03d134ac3260e6485e4655f244bdae9b
Cecile Pham <sheshiru.van@gmail.com>
2019-06-28 10:20:35 +0200

    signup login & css

--- models/User.js
+++ models/User.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true },
   password: {
     type: String,
-    required: true,
-    validate: [
-      val => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(val),
-      "the provided password is not valid !"
-    ]
+    required: true
+    // validate: [
+    //   val => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(val),
+    //   "the provided password is not valid !"
+    // ]
   }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,76 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/login", (req, res, next) => {
-  res.render("login");
-});
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
-});
-// User model
-const UserLog = require("../models/UserLog");
+//-------------------------------------------------------
+// LOGIN PART
+//-------------------------------------------------------
+const UserLog = require("../models/UserLog"); // UserLog model
 // BCrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
 
 router.post("/login", (req, res, next) => {
-  const username = req.body.username;
+  const email = req.body.email;
   const password = req.body.password;
   const salt = bcrypt.genSaltSync(bcryptSalt);
   const hashPass = bcrypt.hashSync(password, salt);
 
   UserLog.create({
-    username,
+    email,
     password: hashPass
   })
     .then(() => {
       if (email === "" || password === "") {
         res.render("login", {
-          errorMessage: "Indicate a username and a password to sign up"
+          errorMessage: "Indicate an email and a password to sign up"
+        });
+        return;
+      }
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+//-------------------------------------------------------
+// SIGNUP PART
+//-------------------------------------------------------
+
+const User = require("../models/User"); // UserLog model
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  User.findOne({ username: username }).then(user => {
+    if (user !== null) {
+      res.render("signup", {
+        errorMessage: "The username already exists!"
+      });
+      return;
+    }
+  });
+
+  User.create({
+    username,
+    lastname,
+    email,
+    password: hashPass
+  })
+    .then(() => {
+      if (username === "" || password === "") {
+        res.render("signup", {
+          errorMessage: "Indicate an email and a password to sign up"
         });
         return;
       }
